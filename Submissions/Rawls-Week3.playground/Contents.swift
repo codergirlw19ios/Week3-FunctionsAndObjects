7ab3ea55ebdd2ea91e16e276848ab70fea79be3b
Amanda Rawls <22384389+amandarawls@users.noreply.github.com>
2019-03-18 23:00:02 -0500

    teacher's solution

--- Submissions/Rawls-Week3.playground/Contents.swift
+++ Submissions/Rawls-Week3.playground/Contents.swift
@@ -4,10 +4,10 @@
 //:
 //:  Write a custom argument label before the parameter name, or write `_` to use no argument label.
 //:
-func greet(_ person: String, on day: String) -> String {
-    return "Hello \(person), today is \(day)."
+func greet(_ person: String, with lunch: String) -> String {
+    return "Hello \(person), today's lunch special is \(lunch)."
 }
-greet("John", on: "Wednesday")
+greet("John", with: "spaghetti")
 
 //: 2.)
 //:
@@ -17,14 +17,13 @@ var numbers = [20, 19, 7, 12]
 
 //: full "syntactic sugar" form:
 numbers.map({ (number: Int) -> Int in
-    let result = 3 * number
-    return result
+    return number % 2 == 0 ? 3 * number : 0
 })
 //: omitting parameters and return type form:
-let mappedNumbers = numbers.map({ number in 3 * number })
+let mappedNumbers = numbers.map({ number in number % 2 == 0 ? 3 * number : 0 })
 print(mappedNumbers)
 //: reference variable form:
-let mappedNums = numbers.map { 3 * $0 }
+let mappedNums = numbers.map { $0 % 2 == 0 ? 3 * $0 : 0 }
 print(mappedNums)
 //: ## Objects and Classes
 //: 3.)
@@ -36,9 +35,20 @@ print(mappedNums)
 //: This version of the `Shape` class is missing something important: an initializer to set up the class when an instance is created. Use `init` to create one.
 //:
 class Shape {
+    let constant: String
+
     var numberOfSides = 0
+
+    init(constant: String) {
+        self.constant = constant
+    }
+
     func simpleDescription() -> String {
         return "A shape with \(numberOfSides) sides."
+    }
+
+    func method(argument: String) {
+
     }
 }
 
@@ -59,6 +69,23 @@ class NamedShape {
     }
 }
 
+
+class Circle: NamedShape {
+    let radius: Double
+
+    init(radius: Double, name: String) {
+        self.radius = radius
+        super.init(name: name)
+    }
+
+    override func simpleDescription() -> String {
+        return "A circle is a shape with no sides."
+    }
+
+    func area() {
+        print("I'm too lazy to calculate an area for \(radius)")
+    }
+}
 //: ## Enumerations and Structures
 //: 6.)
 //:
@@ -87,11 +114,14 @@ enum Rank: Int, CaseIterable {
 let ace = Rank.ace
 let aceRawValue = ace.rawValue
 
+func compare(_ first: Rank, and second: Rank) -> Bool {
+    return first.rawValue > second.rawValue
+}
 //: 7.)
 //:
 //: Add a `color()` method to `Suit` that returns “black” for spades and clubs, and returns “red” for hearts and diamonds.
 //:
-enum Suit {
+enum Suit: CaseIterable {
     case spades, hearts, diamonds, clubs
     
     func simpleDescription() -> String {
@@ -106,10 +136,19 @@ enum Suit {
             return "clubs"
         }
     }
+
+    func color() -> String {
+        switch self {
+        case .spades, .clubs:
+            return "black"
+        case .hearts, .diamonds:
+            return "red"
+        }
+    }
 }
 let hearts = Suit.hearts
 let heartsDescription = hearts.simpleDescription()
-
+hearts.color()
 //: 8.)
 //:
 //: Add a third case to `ServerResponse` and to the switch.
@@ -117,16 +156,20 @@ let heartsDescription = hearts.simpleDescription()
 enum ServerResponse {
     case result(String, String)
     case failure(String)
+    case gamble(Rank)
 }
 
 let success = ServerResponse.result("6:00 am", "8:09 pm")
 let failure = ServerResponse.failure("Out of cheese.")
+let internalError = ServerResponse.gamble(.ace)
 
 switch success {
 case let .result(sunrise, sunset):
     print("Sunrise is at \(sunrise) and sunset is at \(sunset).")
 case let .failure(message):
     print("Failure...  \(message)")
+case let .gamble(rank):
+    print("Never bet on \(rank)")
 }
 
 //: 9.)
@@ -142,3 +185,16 @@ struct Card {
 }
 let threeOfSpades = Card(rank: .three, suit: .spades)
 let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+func deckOfCards() -> [Card] {
+
+    var deck = [Card]()
+
+    for suit in Suit.allCases {
+        for rank in Rank.allCases {
+            deck.append(Card(rank: rank, suit: suit))
+        }
+    }
+
+    return deck
+}
